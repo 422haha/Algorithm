c6f1c2658d818996c0dc25d51fa9a762cf400131
422haha <157022422+422haha@users.noreply.github.com>
2024-01-28 13:29:19 +0900

    [Gold V] Title: 화살을 쏘자!, Time: 4516 ms, Memory: 51876 KB -BaekjoonHub

---
+++ 백준/Gold/20157. 화살을 쏘자！/화살을 쏘자！.py
@@ -0,0 +1,37 @@
+n = int(input())
+gradient = []
+num = {}
+
+def gcd(num_1, num_2):
+    while num_2 != 0:
+        gradi = num_1 % num_2
+        num_1, num_2 = num_2, gradi
+    return abs(num_1)
+
+for i in range(n):
+    x, y = map(int, input().split())
+    if x == 0 and y == 0:
+        continue
+    elif x == 0:
+        if y > 0:
+            gradient.append('+y')
+        else:
+            gradient.append('-y')
+    elif y == 0:
+        if x > 0:
+            gradient.append('+x')
+        else:
+            gradient.append('-x')
+    else:
+        g = gcd(x, y)
+        x //= g
+        y //= g
+        gradient.append((x, y))
+
+for j in gradient:
+    if j in num:
+        num[j] += 1
+    else:
+        num[j] = 1
+
+print(max(num.values()))
