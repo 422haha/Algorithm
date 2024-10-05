7eb36e17fc6b9efa5612ed8b0fdf1900efbc7764
422haha <157022422+422haha@users.noreply.github.com>
2024-10-05 20:44:03 +0900

    [Bronze IV] Title: Which Alien?, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.py
@@ -0,0 +1,9 @@
+a = int(input())
+e = int(input())
+
+if a > 2 and e < 5:
+    print('TroyMartian')
+if a < 7 and e > 1:
+    print('VladSaturnian')
+if a < 3 and e < 4:
+    print('GraemeMercurian')
