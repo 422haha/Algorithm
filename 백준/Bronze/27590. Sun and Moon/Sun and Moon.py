5f5d923b3261c588218fdcf9690887a0062d5fb7
Miyeon Kang <422haha@naver.com>
2025-03-16 22:56:45 +0900

    [Bronze IV] Title: Sun and Moon, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27590. Sun and Moon/Sun and Moon.py
@@ -0,0 +1,13 @@
+ds, ys = map(int, input().split())
+dm, ym = map(int, input().split())
+
+s = ys-ds
+m = ym-dm
+
+while s != m:
+    if s < m:
+        s += ys
+    else:
+        m += ym
+
+print(s)
