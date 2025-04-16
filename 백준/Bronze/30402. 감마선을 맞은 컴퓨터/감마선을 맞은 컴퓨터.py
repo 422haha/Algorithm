75bd07470c98ac59473496a381e6bd91a3a28bac
Miyeon Kang <422haha@naver.com>
2025-04-16 22:27:41 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,10 @@
+for _ in range(15):
+    s = input()
+    if 'w' in s:
+        res = "chunbae"
+    elif 'b' in s:
+        res = "nabi"
+    elif 'g' in s:
+        res = "yeongcheol"
+
+print(res)
