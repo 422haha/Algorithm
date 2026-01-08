02df5f344ef578163d5ac482b9e4ba4da9ec8921
Miyeon Kang <422haha@naver.com>
2026-01-08 21:40:26 +0900

    [Bronze III] Title: Cuboids, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/7581. Cuboids/Cuboids.py
@@ -0,0 +1,13 @@
+while True :
+    l, w, h, v = map(int, input().split())
+    if l == w == h == v == 0 :
+        break
+    if l == 0 :
+        l = v//(w*h)
+    elif w == 0 :
+        w = v//(l*h)
+    elif h == 0 :
+        h = v//(l*w)
+    else :
+        v = l*w*h
+    print(l, w, h, v)
