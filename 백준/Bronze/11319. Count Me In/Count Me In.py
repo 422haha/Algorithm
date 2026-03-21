ab83d27abb557393504528ae2ef566ed5f8b122e
Miyeon Kang <422haha@naver.com>
2026-03-21 22:58:05 +0900

    [Bronze III] Title: Count Me In, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/11319. Count Me In/Count Me In.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    s = input().lower()
+    consonants, vowels = 0, 0
+    for c in s:
+        if c in 'aeiou':
+            vowels += 1
+        elif not c == ' ':
+            consonants += 1
+    print(consonants, vowels)
