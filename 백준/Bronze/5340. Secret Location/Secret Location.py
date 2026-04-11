8ffc08087d1b8037f999c82680a8aa9ab07531e5
Miyeon Kang <422haha@naver.com>
2026-04-11 20:48:42 +0900

    [Bronze III] Title: Secret Location, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/5340. Secret Location/Secret Location.py
@@ -0,0 +1,9 @@
+x1 = len(input().rstrip(' '))
+x2 = len(input().rstrip(' '))
+x3 = len(input().rstrip(' '))
+x4 = len(input().rstrip(' '))
+x5 = len(input().rstrip(' '))
+x6 = len(input().rstrip(' '))
+
+print(f"Latitude {x1}:{x2}:{x3}")
+print(f"Longitude {x4}:{x5}:{x6}")
